0f1a8c438fbce7b4b6456ee8c04f80a48bfdb324
b2stry <liupei521@live.com>
2018-04-11 12:25:40 +0800

    Initial commit

---
+++ shallowan-security-demo/src/main/java/cn/shallowan/security/DemoApplication.java
@@ -0,0 +1,22 @@
+package cn.shallowan.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ShallowAn
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
